71afdd11c48f15accea3b52e909e2d412b065980
Ethan Balgobin <ethanbalgo@hotmail.com>
2026-02-14 01:31:59 +0000

    codecrafters submit [skip ci]

--- src/main.cs
+++ src/main.cs
@@ -2,10 +2,14 @@ class Program
 {
     static void Main()
     {
+        bool run = true;
+
+        while (run)
+        {
         Console.Write("$ ");
 
         string command = Console.ReadLine();
-
         Console.WriteLine($"{command}: command not found");
+        }
     }
 }
